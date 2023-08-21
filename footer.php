a93c99972ac7a2a4022c0864bde09e6cabb1416c
atsuko-fujikawa <atsufuji23@gmail.com>
2023-08-21 10:40:24 +0900

    js→jQuery

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <!-- JS -->
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?v=20230821"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 
 <?php wp_footer(); ?>
