a585a14e8f6c32c04580c8d08408f711c5f68f51
atsuko-fujikawa <atsufuji23@gmail.com>
2023-08-21 10:34:31 +0900

    戻す

--- js/main.js
+++ js/main.js
@@ -1,63 +1,44 @@
-// サイドメニュー
 document.querySelectorAll('.nav-link').forEach(link => {
   if(link.href === window.location.href){
     link.classList.add('active');
   }
 });
 
-// selectorに該当するタブを表示する関数
 
+/**
+ * selectorに該当するタブを表示する関数
+ */
 const showTab = (selector) => {
-
   // 引数selectorの中身をコンソールで確認する
   console.log(selector);
 
   /* 1. タブの選択状態のリセット */
 
-  // いったん、すべてのliからactiveクラスを削除する
-  const tabList = document.getElementsByClassName('works-menu__tab');
-  for (let i = 0; i < tabList.length; i++) {
-    tabList[i].classList.remove('active');
-  }
+  // いったん、すべての.tabs-menu > liからactiveクラスを削除する
+  $('.works-menu > li').removeClass('active');
 
-  // いったん、すべての.worksを非表示にする
-  const tabWorks = document.getElementsByClassName('works');
-  for (let i = 0; i < tabWorks.length; i++) {
-    tabWorks[i].style.display = 'none';
-  }
+  // いったん、すべての.tabs-content > sectionを非表示にする
+  $('.works').hide();
 
   /* 2. 選択されたタブの表示 */
-
   // a要素のhref属性がselectorと一致するものの親要素にだけactiveクラスを付ける
-  const tabA = document.getElementsByClassName('works-menu__a');
-  for (let i = 0; i < tabA.length; i++) {
-    const any = tabA[i].href.split('#');
-    const url = '#' + any[1];
+  $(`.works-menu a[href="${selector}"]`)
+    .parent()
+    .addClass('active');
 
-    if (url === selector) {
-      tabA[i].parentElement.classList.add('active');
-    }
-  }
   // .tabs-content > sectionのうち、selectorに該当するものだけを表示する
-  const selectC = document.getElementById(selector.substr(1));
-  selectC.style.display = 'block';
-
+  $(selector).show();
 };
 
 // タブがクリックされたらコンテンツを表示
-const tabButton = document.getElementsByClassName('works-menu__a');
+$('.works-menu a').on('click', (e) => {
+  // hrefへのページ遷移を止める
+  e.preventDefault();
 
-for (let i = 0; i < tabButton.length; i++) {
-
-  tabButton[i].addEventListener('click',(e) => {
-
-    e.preventDefault();
-    // hrefの値を受け取った後、showTab()関数に渡す。e.targetはクリックされたタブを表す
-    const any = tabButton[i].href.split('#')
-    const selector = '#' + any[1];
-    showTab(selector);
-  });
-}
+  // hrefの値を受け取った後、showTab()関数に渡す。e.targetはクリックされたタブ（.tabs-menu a）を表す
+  const selector = $(e.target).attr('href');
+  showTab(selector);
+});
 
 // 初期状態として1番目のタブを表示
 showTab('#outsourcing');
